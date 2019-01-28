45361826acef3177ad8cf0199e51764eec708694
Dominik Schmidt <dev@dominik-schmidt.de>
2019-01-28 12:06:41 +0100

    Switch license to LGPL 2.1.
    I talked to everyone mentioned in copyright headers and asked people I know from the commit log. If you claim copyright and disagree with this change please let me know.

--- src/libcrashreporter-handler/Handler.cpp
+++ src/libcrashreporter-handler/Handler.cpp
@@ -1,20 +1,22 @@
 /*
- *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
- *   Copyright 2014,      Dominik Schmidt <domme@tomahawk-player.org>
- *   Copyright 2016,      Teo Mrnjavac <teo@kde.org>
+ * libcrashreporter-qt
  *
- *   libcrashreporter is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * Copyright (C) 2010-2011  Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ * Copyright (C) 2014       Dominik Schmidt <dev@dominik-schmidt.de>
+ * Copyright (C) 2016       Teo Mrnjavac <teo@kde.org>
  *
- *   Tomahawk is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *   GNU General Public License for more details.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Handler.h"
